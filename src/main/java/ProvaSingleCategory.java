c94e29e694ef6d7726c22897fac3959680c76fef
Tommaso <tommaso.brini@studio.unibo.it>
2021-07-15 15:43:31 +0200

    Working

---
+++ src/main/java/ProvaSingleCategory.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import utilities.Utilities;
+import view.ViewSwitcher;
+import view.ViewType;
+
+public class ProvaSingleCategory extends Application{
+
+	public static void main(String[] args) throws SQLException, IOException {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		ViewSwitcher.getInstance().switchView(primaryStage, ViewType.START);
+	}
+}
--- src/main/java/view/SingleCategoryView.java
+++ src/main/java/view/SingleCategoryView.java
@@ -1,11 +1,55 @@
 package view;
 
+import entity.Person;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.VBox;
+
 public class SingleCategoryView extends ViewImpl{
 
+    @FXML
+    private VBox vBoxTable;
+    @FXML
+    private TableView<Person> tableView;
+
+    @FXML
+    private TableColumn<Person, String> columnCFPlayer;
+    
+    @FXML
+    private TableColumn<Person, String> columnNamePlayer;
+
+    @FXML
+    private TableColumn<Person, String> columnRuoloPlayer;
+	
+    @FXML
+    private TableColumn<Person, String> columnCFStaff;
+    
+    @FXML
+    private TableColumn<Person, String> columnNameStaff;
+
+    @FXML
+    private TableColumn<Person, String> columnRuoloStaff;
+	
+    @FXML
+    private Label categoria;
+    
+    @FXML
+    private Button addPlayer;
+    
+    @FXML
+    private Button addStaff;
+    
 	@Override
 	public void init() {
 		// TODO Auto-generated method stub
 		
+	}
+	
+	public void prova() {
+		System.out.println("coglione");
 	}
 
 }
