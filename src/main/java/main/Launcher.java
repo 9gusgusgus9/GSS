55d6567500e82bc9c7aba1be6a560bf83a86bd8e
riccardo.rambaldi4 <riccardo.rambaldi4@studio.unibo.it>
2021-08-24 16:37:35 +0200

    create main package

---
+++ src/main/java/main/Launcher.java
@@ -0,0 +1,28 @@
+package main;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import utilities.Utilities;
+import view.ViewSwitcher;
+import view.ViewType;
+
+public class Launcher extends Application{
+
+	public static void main(String[] args) throws SQLException, IOException {
+
+		launch(args);
+		 
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		if(Utilities.isTheFirstStart()) {
+			ViewSwitcher.getInstance().switchView(primaryStage, ViewType.START);
+		} else {
+			ViewSwitcher.getInstance().switchView(primaryStage, ViewType.CATEGORY);
+		}
+	}
+}
